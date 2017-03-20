505f1bd04fda4368d1a6791c564b30a7672d89c9
Luis Correa <lcorrea@lcorrea-ltm.internal.salesforce.com>
2017-03-19 23:06:30 -0300

    clean source

--- src/main/java/br/com/odontoprev/mashup/poc/sales/LiberarSenha.java
+++ src/main/java/br/com/odontoprev/mashup/poc/sales/LiberarSenha.java
@@ -2,6 +2,7 @@ package br.com.odontoprev.mashup.poc.sales;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,7 +25,9 @@ public class LiberarSenha extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("/resultado.jsp");
+		request.setAttribute("action","Solicitar Segunda Via");
+		RequestDispatcher rd = getServletContext().getRequestDispatcher("/resultado.jsp");
+		rd.forward(request,response);
 	}
 
 }
